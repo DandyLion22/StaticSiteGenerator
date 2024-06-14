246a35186696fab88c3c04997c1d0df6c4058584
Daniel Lappe <daniellappe@icloud.com>
2024-06-14 19:33:06 +0200

    Changed server.py and imagename

--- server.py
+++ server.py
@@ -2,20 +2,23 @@ import os
 import argparse
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
-
 def run(
     server_class=HTTPServer,
     handler_class=SimpleHTTPRequestHandler,
     port=8888,
-    directory=None,
+    directory="public",
 ):
     if directory:  # Change the current working directory if directory is specified
+        print(f"Changing directory to {directory}")
         os.chdir(directory)
+        print(f"Current working directory: {os.getcwd()}")
+        print(f"Directory contents: {os.listdir()}")
+        if 'images' in os.listdir():
+            print(f"Images directory contents: {os.listdir('images')}")
     server_address = ("", port)
     httpd = server_class(server_address, handler_class)
     print(f"Serving HTTP on http://localhost:{port} from directory '{directory}'...")
     httpd.serve_forever()
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="HTTP Server")
--- src/main.py
+++ src/main.py
@@ -4,10 +4,10 @@ import shutil
 from copystatic import copy_files_recursive
 from gencontent import generate_page
 
-dir_path_static = "./static"
-dir_path_public = "./public"
-dir_path_content = "./content"
-template_path = "./template.html"
+dir_path_static = "/Users/daniellappe/workspace/github.com/DandyLion22/staticsitegenerator/static"
+dir_path_public = "/Users/daniellappe/workspace/github.com/DandyLion22/staticsitegenerator/public"
+dir_path_content = "/Users/daniellappe/workspace/github.com/DandyLion22/staticsitegenerator/content"
+template_path = "/Users/daniellappe/workspace/github.com/DandyLion22/staticsitegenerator/src/template.html"
 
 
 def main():
